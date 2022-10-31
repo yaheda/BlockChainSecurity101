b36c8d89679301cb80fb56eae38eb3a4abf6e8f3
yaheda <yaheda.a@gmail.com>
2022-11-01 00:16:57 +0200

    update ethdex errors

--- 11FinalProject/contracts/EtbDex.sol
+++ 11FinalProject/contracts/EtbDex.sol
@@ -3,29 +3,37 @@ pragma solidity 0.8.9;
 
 import { ETBToken } from "./EtbToken.sol";
 
-contract EtbDex {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract EtbDex is Ownable {
+  using Address for address payable;
+
   address public owner;
   ETBToken private _etbToken;
   uint256 public fee;
-  bytes32 private password; /// anyone can see this password
+  //bytes32 private password; /// anyone can see this password
 
   constructor(address _token, bytes32 _password) public {
     _etbToken = ETBToken(_token);
-    password = _password;
-    owner = msg.sender;
+    //password = _password;
+   // owner = msg.sender;
   }
 
-  /// this owner should be the deployer
-  modifier onlyOwner(bytes32 _password) {
-    require(password == _password, "You are not the owner!");
-    _;
-  }
+  // /// this owner should be the deployer
+  // modifier onlyOwner(bytes32 _password) {
+  //   require(password == _password, "You are not the owner!");
+  //   _;
+  // }
 
   
   /// overflow
   function buyTokens() external payable {
     require(msg.value > 0, "Should send ETH to buy tokens");
-    require(_etbToken.balanceOf(owner) - msg.value >= 0, "Not enough tokens to sell");
+    require(_etbToken.balanceOf(owner) >= msg.value, "Not enough tokens to sell");
+
     _etbToken.transferFrom(owner, msg.sender, msg.value - calculateFee(msg.value));
   }
 
@@ -34,14 +42,15 @@ contract EtbDex {
   function sellTokens(uint256 _amount) external {
     require(_etbToken.balanceOf(msg.sender) - _amount >= 0, "Not enough tokens");
 
-    payable(msg.sender).send(_amount);
+    //payable(msg.sender).send(_amount);
+    payable(msg.sender).sendValue(_amount);
 
     _etbToken.burn(msg.sender, _amount);
     _etbToken.mint(_amount);
   }
 
   /// set fee using owner
-  function setFee(uint256 _fee, bytes32 _password) external onlyOwner(_password) {
+  function setFee(uint256 _fee) external onlyOwner() {
     fee = _fee;
   }
 
@@ -51,7 +60,10 @@ contract EtbDex {
   }
 
   /// address.sendvalue
-  function withdrawFees(bytes32 _password) external onlyOwner(_password) {
-    payable(msg.sender).send(address(this).balance);
+  function withdrawFees() external onlyOwner() {
+    //payable(msg.sender).send(address(this).balance);
+    payable(msg.sender).sendValue(address(this).balance);
   }
+
+  receive() external { }
 }
